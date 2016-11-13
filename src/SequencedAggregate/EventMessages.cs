76c4e47267f2087a4b62b63c57f379d099cf8ff6
Aejmelaeus <aejmelaeus@Aejmelaeuss-MBP.lan>
2016-11-13 09:23:53 +0100

    Updated readme

--- src/SequencedAggregate/EventMessages.cs
+++ src/SequencedAggregate/EventMessages.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Generic;
-using NEventStore;
+﻿using NEventStore;
+using System.Collections.Generic;
 
 namespace SequencedAggregate
 {
     public static class EventMessages
     {
-        public static IEnumerable<EventMessage> Parse<TEvent>(Dictionary<long, IEnumerable<TEvent>> events)
+        public static IEnumerable<EventMessage> Parse(long sequenceAnchor, IEnumerable<object> events)
         {
-            return null;
+            var result = new List<EventMessage>();
+
+            var index = 0;
+
+            foreach (var @event in events)
+            {
+                var eventMessage = new EventMessage
+                {
+                    Body = @event,
+                    Headers = new Dictionary<string, object>
+                    {
+                        { SequenceConstants.SequenceAnchorKey, sequenceAnchor },
+                        { SequenceConstants.AnchorIndexKey, index }
+                    }
+                };
+
+                index++;
+
+                result.Add(eventMessage);
+            }
+
+            return result;
         }
     }
 }
